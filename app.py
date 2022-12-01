ceee8b9ea3e8d8015f7f20fe3e38dad1c214c272
djrobin088 <81708365+djrobin088@users.noreply.github.com>
2022-12-01 21:14:09 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from instagrapi import Client
+import os
+import time
+
+my_bot = Client()
+
+ins_caption = "-\n-\n𝓢𝓬𝓻𝓸𝓵𝓵 →\n\n\n(¯´•._.• 🎀 𝐸𝓂❀𝒿𝒾 → 🎀 •._.•´¯( \n\n░L░o░a░d░i░n░g░ \n\n_____🐼👊  ιᶰţ𝒆Ⓡ乇ⓢ𝐭𝔦ŇᎶ  🏆🍭______ \n.\n. \nｈａｓｈｔａｇｓ \n- \n- \n- \n#trending #viral #instagram #explore #instagood #love #tiktok #explorepage #fashion #tiktok #reels #beautiful #cute #follow #selfie #art #friends #nature #girl #style #blogger #food #travel #photography #photooftheday #instadaily #fitness #me #fun #smile "
+my_bot.login(username="iftekharalammithu23", password="mithuboomboom")
+
+img_dir = "imgs"
+
+for i in os.listdir(img_dir):
+    img_path = os.path.join(img_dir, i)
+    my_bot.photo_upload(path=f'{img_path}', caption=ins_caption)
+    # Delete Uploaded File
+    try:
+        os.remove(img_path)
+    except:
+        pass
+    #sleep For 4.8 Hours
+    time.sleep(17280)
