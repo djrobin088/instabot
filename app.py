bc24d80e06c2d23e1fe738860d82975934aa9ce9
djrobin088 <81708365+djrobin088@users.noreply.github.com>
2022-12-02 09:56:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 my_bot = Client()
 
 ins_caption = "-\n-\n𝓢𝓬𝓻𝓸𝓵𝓵 →\n\n\n(¯´•._.• 🎀 𝐸𝓂❀𝒿𝒾 → 🎀 •._.•´¯( \n\n░L░o░a░d░i░n░g░ \n\n_____🐼👊  ιᶰţ𝒆Ⓡ乇ⓢ𝐭𝔦ŇᎶ  🏆🍭______ \n.\n. \nｈａｓｈｔａｇｓ \n- \n- \n- \n#trending #viral #instagram #explore #instagood #love #tiktok #explorepage #fashion #tiktok #reels #beautiful #cute #follow #selfie #art #friends #nature #girl #style #blogger #food #travel #photography #photooftheday #instadaily #fitness #me #fun #smile "
-my_bot.login(username="iftekharalammithu23", password="mithuboomboom")
+my_bot.login(username="*****", password="*****")
 
 img_dir = "imgs"
 
